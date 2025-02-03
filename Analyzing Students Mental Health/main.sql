fd90efcf663ea1c90ab850d0dc06e9ccfbf02d16
Abhishek Boga <abhishekboga99@gmail.com>
2025-02-03 19:14:16 +0530

    formatted sql query

--- Analyzing Students Mental Health/main.sql
+++ Analyzing Students Mental Health/main.sql
@@ -7,7 +7,12 @@
 -- FROM students;
 
 -- main query
-select stay, count(inter_dom) as count_int, round(avg(todep),2) as average_phq, round(avg(tosc),2) as average_scs, round(avg(toas),2) as average_as
+select 
+    stay, 
+    count(inter_dom) as count_int, 
+    round(avg(todep),2) as average_phq, 
+    round(avg(tosc),2) as average_scs, 
+    round(avg(toas),2) as average_as
 from students
 where inter_dom='Inter'
 group by stay
